fa8935071607cc1a660f8cabbfd69785a1d12da4
Vladyslav Rudniev <Rudniev.CSdev@gmail.com>
2022-02-11 15:59:47 +0200

    021

---
+++ task4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            MyList<string> list = new MyList<string>();
+            list.Add("Alex");
+            list.Add("Genry");
+            list.Add("Roman");
+
+            string[] array = list.GetArray();
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine(array[i]);
+        }
+    }
+}
